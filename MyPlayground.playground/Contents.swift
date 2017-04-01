8f5f2eb65978ae62f97cb23950df63a7379a16f9
CrimsonOHara <parikhsachi@gmail.com>
2017-04-01 15:34:51 -0700

    Make thief jump

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,16 +8,16 @@ enum GameSceneState {
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate   {
-    var gameState: GameSceneState = .active
+    var gameState: GameSceneState = .gameOver
     var timer: CGFloat = 0
     var sinceTouch : CFTimeInterval = 0
     var scoreLabel: SKLabelNode!
     let fixedDelta: CFTimeInterval = 1.0/60.0 /* 60 FPS */
     var light: SKReferenceNode!
+    var thief: SKNode!
     var coins: SKSpriteNode!
     var block: SKSpriteNode!
     var points = 0
-    var thief: SKSpriteNode!
     var scrollLayer: SKNode!
     let scrollSpeed: CGFloat = 2
     var shield: SKSpriteNode!
@@ -29,21 +29,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate   {
     override func didMove(to view: SKView) {
     self.physicsWorld.contactDelegate = self
     buttonStart = self.childNode(withName: "buttonStart")!
+    thief = self.childNode(withName: "//actualthief")!
+
     }
     func didBegin(_ contact: SKPhysicsContact) {
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            if buttonStart.contains(location) || self.atPoint(location).name == "buttonStart" {
-                buttonStart.alpha = 0
-                gameState = .active
+        if gameState != .active {
+            for touch in touches {
+                let location = touch.location(in: self)
+                if buttonStart.contains(location) || self.atPoint(location).name == "buttonStart" {
+                    buttonStart.alpha = 0
+                    gameState = .active
+                }
             }
+        return
         }
+        thief.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 20))
+        thief.physicsBody?.categoryBitMask = 4
+        thief.physicsBody?.collisionBitMask = 4
     }
 
-    
 }
 
 let scene = GameScene(fileNamed: "GameScene")
